6a8ec6b7b2df6f7d60f893da026172c42d8b0957
Antipov Mikhail <orangelemon2007@yandex.ru>
2023-03-28 12:07:31 +0300

    [DeckEmul] added VolumeUp/Down components

--- src/StreamDeck/Transport/FakeDevice.cpp
+++ src/StreamDeck/Transport/FakeDevice.cpp
@@ -1,5 +1,3 @@
-
-
 #include "FakeDevice.h"
 
 FakeDevice::FakeDevice() {
--- src/modules/ALSA/main.cpp
+++ src/modules/ALSA/main.cpp
@@ -12,12 +12,15 @@
 DECLARE_MODULE(AlsaModule)
 DEFINE_MODULE(AlsaModule)
 
-#include "image_mute.h"
-#include "image_unmute.h"
+#define ALSA_VOLUME_UP_STEP     (5)
+#define ALSA_VOLUME_DOWN_STEP   (5)
 
-class MuteComponent : public IComponent
+#include "image_bytes.h"
+
+
+class VolumeMuteComponent : public IComponent
 {
-    DECLARE_MODULE_COMPONENT(AlsaModule, MuteComponent)
+    DECLARE_MODULE_COMPONENT(AlsaModule, VolumeMuteComponent)
 
 public:
     void init(std::shared_ptr<IStreamDeck> deck) override
@@ -94,4 +97,161 @@ private:
     }
 };
 
-REGISTER_MODULE_COMPONENT(AlsaModule, MuteComponent)
+REGISTER_MODULE_COMPONENT(AlsaModule, VolumeMuteComponent)
+
+class VolumeDownComponent : public IComponent
+{
+    DECLARE_MODULE_COMPONENT(AlsaModule, VolumeDownComponent)
+
+public:
+    void init(std::shared_ptr<IStreamDeck> deck) override
+    {
+        image::helper::TargetImageParameters image_params =
+        {
+            deck->key_image_format().size.first,
+            deck->key_image_format().size.second,
+            deck->key_image_format().flip.first,
+            deck->key_image_format().flip.second
+        };
+        m_image = image::helper::prepareImageForDeck(IMAGE_VOLUME_DOWN, image_params);
+    }
+
+    std::string name() const override
+    {
+        return "VolumeDown";
+    }
+
+    void tick() override
+    {
+
+    }
+
+    void actionPress(std::shared_ptr<IStreamDeck> deck, ushort key) override
+    {
+        SetVolumeDown(ALSA_VOLUME_DOWN_STEP);
+    }
+
+    void actionRelease(std::shared_ptr<IStreamDeck> deck, ushort key) override
+    {
+
+    }
+
+    std::vector<unsigned char> getImage() const override
+    {
+        return m_image;
+    }
+
+private:
+    std::vector<unsigned char> m_image;
+
+    void SetVolumeDown(long step)
+    {
+        long min{}, max{};
+        snd_mixer_t *handle = nullptr;
+        snd_mixer_selem_id_t *sid = nullptr;
+        const char *card = "default";
+        const char *selem_name = "Master";
+
+        snd_mixer_open(&handle, 0);
+        snd_mixer_attach(handle, card);
+        snd_mixer_selem_register(handle, NULL, NULL);
+        snd_mixer_load(handle);
+
+        snd_mixer_selem_id_alloca(&sid);
+        snd_mixer_selem_id_set_index(sid, 0);
+        snd_mixer_selem_id_set_name(sid, selem_name);
+        snd_mixer_elem_t *elem = snd_mixer_find_selem(handle, sid);
+
+        snd_mixer_selem_get_playback_volume_range(elem, &min, &max);
+
+        long curr_volume {};
+        snd_mixer_selem_get_playback_volume(elem, SND_MIXER_SCHN_MONO, &curr_volume);
+        if (curr_volume > min)
+        {   const long step_value = (max - min) * step / 100;
+            curr_volume = std::max(min, curr_volume - step_value);
+            snd_mixer_selem_set_playback_volume_all(elem, curr_volume);
+        }
+        snd_mixer_close(handle);
+    }
+};
+
+REGISTER_MODULE_COMPONENT(AlsaModule, VolumeDownComponent)
+
+
+class VolumeUpComponent : public IComponent
+{
+    DECLARE_MODULE_COMPONENT(AlsaModule, VolumeUpComponent)
+
+public:
+    void init(std::shared_ptr<IStreamDeck> deck) override
+    {
+        image::helper::TargetImageParameters image_params =
+        {
+            deck->key_image_format().size.first,
+            deck->key_image_format().size.second,
+            deck->key_image_format().flip.first,
+            deck->key_image_format().flip.second
+        };
+        m_image = image::helper::prepareImageForDeck(IMAGE_VOLUME_UP, image_params);
+    }
+
+    std::string name() const override
+    {
+        return "VolumeUp";
+    }
+
+    void tick() override
+    {
+
+    }
+
+    void actionPress(std::shared_ptr<IStreamDeck> deck, ushort key) override
+    {
+        SetVolumeUp(ALSA_VOLUME_UP_STEP);
+    }
+
+    void actionRelease(std::shared_ptr<IStreamDeck> deck, ushort key) override
+    {
+
+    }
+
+    std::vector<unsigned char> getImage() const override
+    {
+        return m_image;
+    }
+
+private:
+    std::vector<unsigned char> m_image;
+
+    void SetVolumeUp(long step)
+    {
+        long min{}, max{};
+        snd_mixer_t *handle = nullptr;
+        snd_mixer_selem_id_t *sid = nullptr;
+        const char *card = "default";
+        const char *selem_name = "Master";
+
+        snd_mixer_open(&handle, 0);
+        snd_mixer_attach(handle, card);
+        snd_mixer_selem_register(handle, NULL, NULL);
+        snd_mixer_load(handle);
+
+        snd_mixer_selem_id_alloca(&sid);
+        snd_mixer_selem_id_set_index(sid, 0);
+        snd_mixer_selem_id_set_name(sid, selem_name);
+        snd_mixer_elem_t *elem = snd_mixer_find_selem(handle, sid);
+
+        snd_mixer_selem_get_playback_volume_range(elem, &min, &max);
+
+        long curr_volume {};
+        snd_mixer_selem_get_playback_volume(elem, SND_MIXER_SCHN_MONO, &curr_volume);
+        if (curr_volume < max)
+        {   const long step_value = (max - min) * step / 100;
+            curr_volume = std::max(min, curr_volume + step_value);
+            snd_mixer_selem_set_playback_volume_all(elem, curr_volume);
+        }
+        snd_mixer_close(handle);
+    }
+};
+
+REGISTER_MODULE_COMPONENT(AlsaModule, VolumeUpComponent)
